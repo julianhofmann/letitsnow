c986f8a8e4b9f9f5f7849144d7b72a38b6e6ab92
Julian Hofmann <info@julian-hofmann.net>
2022-11-27 20:50:12 +0100

    [TASK] CGL in JavaScript

--- Resources/Public/JavaScript/ContentElement.js
+++ Resources/Public/JavaScript/ContentElement.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#letitsnow").on( "click", function () {
-    $("body").letItSnow()
+  $("#letitsnow").on("click", function () {
+    $("body").letItSnow();
   });
 });
